5380e4f7f85cd27c6996a774165de50f046f9a78
missnoelia <noelia7prox@gmail.com>
2024-07-08 16:21:05 +0200

    feat: fetch to pokeapi

---
+++ script.js
@@ -0,0 +1,24 @@
+async function getdata() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+  const json = await response.json();
+
+  console.log(json);
+  //
+  const pokemonList = document.getElementById("pokemons");
+  json.results.forEach(async (pokemon) => {
+    const pokemonItem = document.createElement("div");
+
+    const pokemonResponse = await fetch(pokemon.url);
+    const pokemonJson = await pokemonResponse.json();
+    console.log(pokemonJson);
+
+    pokemonItem.innerHTML = `
+    <p>${pokemon.name}</p>
+    <img src=${pokemonJson.sprites.front_default}>
+    `;
+
+    pokemonList.appendChild(pokemonItem);
+  });
+}
+
+getdata();
